d9d8826822c5c11e82d32953c15d6e00ce5639bf
angelicamolero <angelicamoleror8@gmail.com>
2021-03-28 22:08:42 -0300

    creating folders and principal components, adding router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/auth/Login';
+import NewAccount from './components/auth/NewAccount';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
