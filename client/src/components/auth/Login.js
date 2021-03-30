36ff40d31f52e1073330b15633515eebbee0632a
angelicamolero <angelicamoleror8@gmail.com>
2021-03-29 23:03:47 -0300

    adding component login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,44 @@
 import  React from 'react';
 
 const Login = () => {
+
+    const onChange = () => {
+
+    }
+    
     return (
-        <h1>Login</h1>
+        <div className="form-usuario">
+            <div className="container-form shadow-dark">
+                <h1>Sign In</h1>
+                <form action="">
+                    <div className="field-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Your email"
+                        onChange={onChange}
+                        />
+                    </div>
+                    <div className="field-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Your password"
+                        onChange={onChange}
+                        />
+                    </div>
+                    <div className="field-form">
+                        <input type="submit" className="btn btn-primary btn-block"
+                        value="Sign In"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 };
 
