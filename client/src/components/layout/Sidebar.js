0f565effe904ce85ad9764dd17db1a6bef6a7d21
angelicamolero <angelicamoleror8@gmail.com>
2021-03-31 16:42:14 -0300

    adding state to newproject

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import NewProject from '../projects/NewProject'
 
 const Sidebar = () => {
     return(
         <aside>
             <h1>MERN <span>Tasks</span></h1>
+            <NewProject/>
             <div className="projects">
                 <h2>Your Projects</h2>
             </div>
--- client/src/components/projects/NewProject.js
+++ client/src/components/projects/NewProject.js
@@ -1,6 +1,30 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const NewProject = () => {
+
+    const [ project, saveProject ] = useState({
+        name: ''
+    });
+
+    const { name } = project;
+
+    const onChangeProject = e => {
+        saveProject({
+            ...project,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const onSubmitProject = e => {
+        e.preventDefault();
+
+        //validate project
+
+        //add to state
+
+        //restart form
+    }
+
     return(
         <Fragment>
             <button
@@ -9,13 +33,21 @@ const NewProject = () => {
                 New Project</button>
 
             <form 
-                className="form-new-project">
+                className="form-new-project"
+                onSubmit={onSubmitProject}>
                 <input 
                     type="text"
                     className="input-text"
                     placeholder="Project's Name"
                     name="name"
+                    value={name}
+                    onChange={onChangeProject}
                     />
+                <input 
+                    type="submit"
+                    className="btn btn-block btn-primary"
+                    value="Add Project"   
+                />
             </form>
         </Fragment>
     )
